e2df8587b783e461954709709de6249fa2ebd20c
IHC(MohammadAminFahimi) <fahimi13823182@gmail.com>
2022-07-11 21:12:31 +0430

    test

---
+++ src/SaveToFile.h
@@ -0,0 +1,14 @@
+//
+// Created by IHC on 7/11/2022.
+//
+
+#ifndef PLAYWITHFIRE2_SAVETOFILE_H
+#define PLAYWITHFIRE2_SAVETOFILE_H
+
+
+class SaveToFile {
+
+};
+
+
+#endif //PLAYWITHFIRE2_SAVETOFILE_H
