79315ad090fbec7f66ef122de2bfac9ae4fc8afd
mehrzada47 <mehrzada47@gmail.com>
2022-07-12 23:34:28 +0430

    result design complete2

---
+++ cmake-build-debug/CMakeFiles/PlayWithFire2.dir/src/Controller.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/PlayWithFire2.dir/src/Controller.cpp.obj: \
+ E:\programing\PlayWithFire22\src\Controller.cpp \
+ E:\programing\PlayWithFire22\src\Controller.h
---
+++ cmake-build-debug/CMakeFiles/PlayWithFire2.dir/src/SaveToFile.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/PlayWithFire2.dir/src/SaveToFile.cpp.obj: \
+ E:\programing\PlayWithFire22\src\SaveToFile.cpp \
+ E:\programing\PlayWithFire22\src\SaveToFile.h
